5ed626e7ece52c531ab1d0bc97c1f1f6f95eabb4
Ruslan <115550042+169408@users.noreply.github.com>
2023-10-18 09:25:58 +0200

    Add files via upload

---
+++ Cw_3_isi5/Cw_3_isi5/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Cw_3_isi5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lable_wynik = new System.Windows.Forms.Label();
+            this.lbl_tab = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(325, 280);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lable_wynik
+            // 
+            this.lable_wynik.AutoSize = true;
+            this.lable_wynik.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lable_wynik.Location = new System.Drawing.Point(322, 226);
+            this.lable_wynik.Name = "lable_wynik";
+            this.lable_wynik.Size = new System.Drawing.Size(0, 18);
+            this.lable_wynik.TabIndex = 1;
+            // 
+            // lbl_tab
+            // 
+            this.lbl_tab.AutoSize = true;
+            this.lbl_tab.Location = new System.Drawing.Point(315, 181);
+            this.lbl_tab.Name = "lbl_tab";
+            this.lbl_tab.Size = new System.Drawing.Size(0, 13);
+            this.lbl_tab.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_tab);
+            this.Controls.Add(this.lable_wynik);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lable_wynik;
+        private System.Windows.Forms.Label lbl_tab;
+    }
+}
+
---
+++ Cw_3_isi5/Cw_3_isi5/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cw_3_isi5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int[] sort_buble(int[] tablica)
+        {
+            for(int i = 0; i < tablica.Length; i++)
+            {
+                for(int j = 0; j < tablica.Length - i - 1; j++)
+                {
+                    if (tablica[j] > tablica[j+1])
+                    {
+                        int temp = tablica[j];
+                        tablica[j] = tablica[j + 1];
+                        tablica[j + 1] = temp;
+                    }
+                }
+            }
+            return tablica;
+        }
+
+        private int[] inny_sort(int[] tablica)
+        {
+            bool czyzamiana = false;
+            do
+            {
+                czyzamiana = false;
+                for (int i = 0; i < tablica.Length - 1; i++)
+                {
+                    if (tablica[i] > tablica[i + 1])
+                    {
+                        czyzamiana = true;
+                        int temp = tablica[i];
+                        tablica[i] = tablica[i + 1];
+                        tablica[i + 1] = temp;
+                    }
+                }
+            } while (czyzamiana);
+            return tablica;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 4 7 2 1
+            // 4 2 7 1
+            // 4 2 1 7
+
+            // 2 4 1 7
+            // 2 1 4 7
+
+            // 1 2 4 7
+            int[] tab = { 1, 4, 2, 7, 0, 3 };
+            for (int i = 0; i < tab.Length; i++)
+            {
+                lbl_tab.Text += " " + tab[i] + " ";
+                Console.Write(tab[i] + " ");
+            }
+            int[] sorted_tab = inny_sort(tab);
+            string wynik = "";
+            for(int i = 0; i < sorted_tab.Length; i++)
+            {
+                wynik += sorted_tab[i] + " ";
+                lable_wynik.Text += " " + sorted_tab[i] + " ";
+                Console.Write(sorted_tab[i] + " ");
+            }
+            MessageBox.Show(wynik);
+            Console.WriteLine("\n Koniec ");
+        }
+    }
+}
