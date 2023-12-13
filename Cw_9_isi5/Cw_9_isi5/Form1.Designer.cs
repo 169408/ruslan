6b3afd558dcdaf6a3043c3d29d5b536c78e8fb99
Ruslan <115550042+169408@users.noreply.github.com>
2023-12-13 09:31:32 +0100

    Add files via upload

---
+++ Cw_9_isi5/Cw_9_isi5/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Cw_9_isi5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_str = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_str
+            // 
+            this.btn_str.Location = new System.Drawing.Point(274, 242);
+            this.btn_str.Name = "btn_str";
+            this.btn_str.Size = new System.Drawing.Size(75, 23);
+            this.btn_str.TabIndex = 0;
+            this.btn_str.Text = "Start";
+            this.btn_str.UseVisualStyleBackColor = true;
+            this.btn_str.Click += new System.EventHandler(this.btn_str_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_str);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_str;
+    }
+}
+
